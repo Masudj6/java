1f8fdba2a0835b7a2a676a8b71131acf9cd987f1
MdJone <jone.handymama@gmail.com>
2019-01-26 12:42:42 +0600

    new line add main file

--- src/MainClass.java
+++ src/MainClass.java
@@ -40,11 +40,10 @@ public class MainClass {
 		
 		
 		System.out.println("The given array is:" );
-		
-		
+		System.out.println("The given array is:" );
 		
 		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[0]+"\t");
+			System.out.print(arr[i]+"\t");
 		}
 		
 		
